82b62fd6da0f3fa4cbc3080d9995c6a8d4f264d8
tjkpolisher <jktak5609@gmail.com>
2023-06-27 14:29:33 +0900

    df 구조 수정

--- pages/05_Heatmap.py
+++ pages/05_Heatmap.py
@@ -14,21 +14,13 @@ df = common.get_data()
 df['Measurement date'] = pd.to_datetime(df['Measurement date'])  # 문자열을 날짜/시간 형식으로 변환
 df['hour'] = df.loc[:, "Measurement date"].dt.hour
 df['date'] = df.loc[:, "Measurement date"].dt.date  # 날짜 컬럼 생성
-df['time'] = df.loc[:, "Measurement date"].dt.time  # 시간 컬럼 생성
-
-df['Measurement date'] = df['Measurement date'].astype('str')
-df_date =df['Measurement date'].str.split(" ",n=1,expand=True) # 바로 데이터프레임의 컬럼으로 생성 expand=True
-df_date.head()
-
-df['date'] = df_date[0]
-df['time'] = df_date[1]
-df['Measurement date'] = pd.to_datetime(df['Measurement date'])
 
 df = df.groupby(['date'], as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
 # df['Measurement date'] = pd.to_datetime(df['Measurement date'])
 df['date'] = pd.to_datetime(df['date'])
-df = df.drop(['Measurement date'],axis = 1)
-df = df.drop(['date'],axis = 1)
+df = df.drop(['Measurement date'], axis = 1)
+df = df.drop(['date'], axis = 1)
+
 # Calculate Pearson's correlation coefficient
 df_air = df.corr()
 
