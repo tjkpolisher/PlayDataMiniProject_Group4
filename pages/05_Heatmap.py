fba7df11f6d39f7c77f43675607bd01b577d6fec
tjkpolisher <jktak5609@gmail.com>
2023-06-27 12:41:13 +0900

    추가 수정

--- pages/05_Heatmap.py
+++ pages/05_Heatmap.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 import common
 import numpy as np
 import pandas as pd
@@ -10,13 +11,10 @@ common.page_config()
 st.title("Time-Pollution Material Correlation Coeff. and Heatmap")
 
 df = common.get_data()
-df['Measurement date'] = df['Measurement date'].astype('str')
-df_date =df['Measurement date'].str.split(" ",n=1,expand=True) # 바로 데이터프레임의 컬럼으로 생성 expand=True
-df_date.head()
-
-df['date'] = df_date[0]
-df['time'] = df_date[1]
-df = df.drop(['Measurement date'],axis = 1)
+df['Measurement date'] = pd.to_datetime(df['Measurement date'])  # 문자열을 날짜/시간 형식으로 변환
+df['date'] = df['Measurement date'].dt.date  # 날짜 컬럼 생성
+df['time'] = df['Measurement date'].dt.time  # 시간 컬럼 생성
+df = df.drop(['Measurement date'], axis=1)
 
 df = df.groupby(['date'], as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
 
