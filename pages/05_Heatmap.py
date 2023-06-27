247581dff141e2986b5b4f813072870092f673fc
tjkpolisher <jktak5609@gmail.com>
2023-06-27 14:37:36 +0900

    new

--- pages/05_Heatmap.py
+++ pages/05_Heatmap.py
@@ -12,9 +12,8 @@ st.title("Time-Pollution Material Correlation Coeff. and Heatmap")
 
 df = common.get_data()
 df['Measurement date'] = pd.to_datetime(df['Measurement date'])  # 문자열을 날짜/시간 형식으로 변환
-df['hour'] = df.loc[:, "Measurement date"].dt.hour
 df['date'] = df.loc[:, "Measurement date"].dt.date  # 날짜 컬럼 생성
-df['Measurement date'] = df['Measurement date'].astype('str')
+df = df.drop(['Measurement date'], axis=1)
 
 df = df.groupby(['date'], as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
 # df['Measurement date'] = pd.to_datetime(df['Measurement date'])
